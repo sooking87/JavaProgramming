c6b1e6016e80a91ee440d6323f36d99e381874e2
SooKyoung <sksohn01@naver.com>
2022-04-10 03:33:11 +0900

    commit

---
+++ Chap9/EmptyContainerDemo.java
@@ -0,0 +1,14 @@
+package Chap9;
+
+import java.awt.*;
+
+public class EmptyContainerDemo {
+    Frame f;
+
+    public EmptyContainerDemo() {
+        f = new Frame("Let's Study Java");
+
+        f.setSize(400, 200);
+        f.setVisible(true);
+    }
+}
---
+++ Chap9/EmptyContainerMain.java
@@ -0,0 +1,7 @@
+package Chap9;
+
+public class EmptyContainerMain {
+    public static void main(String[] args) {
+        new EmptyContainerDemo();
+    }
+}
