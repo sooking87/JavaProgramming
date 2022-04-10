64dd46ddfe3c2142416daf7e9f540175c38307c4
SooKyoung <sksohn01@naver.com>
2022-04-10 10:23:12 +0900

    2022-04-10-no_6-commit

---
+++ Chap9/DrawRandomRecByAwt.java
@@ -0,0 +1,31 @@
+package Chap9;
+
+import java.awt.*;
+
+public class DrawRandomRecByAwt {
+    Frame frame;
+    Label label;
+
+    public DrawRandomRecByAwt() {
+        frame = new Frame("Random Labels");
+        frame.setLayout(null);
+
+        for (int i = 0; i < 20; i++) {
+            label = new Label();
+            int x = (int) (Math.random() * 200) + 50;
+            int y = (int) (Math.random() * 200) + 50;
+            System.out.println(x + ", " + y);
+            label.setLocation(x, y);
+            label.setSize(10, 10);
+            label.setBackground(Color.blue); // jlabel.setOpaque(true); jlagel.setBackground(Color.blue);
+            frame.add(label);
+        }
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new DrawRandomRecByAwt();
+    }
+}
