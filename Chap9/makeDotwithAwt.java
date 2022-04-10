66a88bca664ee1aa11c9c924bc72d567ccb01825
SooKyoung <sksohn01@naver.com>
2022-04-10 15:57:59 +0900

    2022-04-10-no_8-commit

---
+++ Chap9/makeDotwithAwt.java
@@ -0,0 +1,61 @@
+package Chap9;
+
+import java.awt.*;
+
+public class makeDotwithAwt {
+    Frame frame;
+    Panel top, mid, bottom;
+    Button open, close, exit, input;
+    TextField inputBox;
+    Label label;
+
+    public makeDotwithAwt() {
+        frame = new Frame("여러 개의 패널을 가진 프레임");
+        frame.setLayout(new BorderLayout());
+        top = new Panel();
+        mid = new Panel();
+        bottom = new Panel();
+
+        // 상단 패널 만들기
+        open = new Button("열기");
+        close = new Button("닫기");
+        exit = new Button("나가기");
+
+        top.add(open); // Panel 디폴트 flowLayout
+        top.add(close);
+        top.add(exit);
+        top.setBackground(Color.lightGray);
+
+        // 중간 패널 만들기
+        for (int i = 0; i < 10; i++) {
+            label = new Label("*");
+            int x = (int) (Math.random() * 400) + 50;
+            int y = (int) (Math.random() * 400) + 50;
+            System.out.println(x + ", " + y);
+            // 6번은 되는데 왜 얘는 랜덤으로 안찍히지,,,,?
+            label.setLocation(x, y);
+            label.setForeground(Color.red);
+            mid.add(label);
+        }
+
+        // 하단 패널 만들기
+        input = new Button("Word Input");
+        inputBox = new TextField(20);
+        bottom.add(input);
+        bottom.add(inputBox);
+        bottom.setBackground(Color.yellow);
+
+        // 윈도우에 패널 붙히기
+        frame.add(top, BorderLayout.NORTH);
+        frame.add(mid, BorderLayout.CENTER);
+        frame.add(bottom, BorderLayout.SOUTH);
+
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new makeDotwithAwt();
+    }
+
+}
